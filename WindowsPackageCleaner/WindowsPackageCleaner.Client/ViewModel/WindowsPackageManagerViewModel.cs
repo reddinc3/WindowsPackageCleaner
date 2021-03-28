35c8f13243e573a779659efedf307a1ccec3be9a
reddinc3 <conor.reddin3@mail.dcu.ie>
2021-03-28 16:01:16 +0100

    Changed TargetFramework from .Net Core 3.1 to .Net Framework 4.8. Created WindowsPackageManagerViewModel.cs file and set up bindings.

---
+++ WindowsPackageCleaner/WindowsPackageCleaner.Client/ViewModel/WindowsPackageManagerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using Windows.ApplicationModel;
+
+namespace WindowsPackageCleaner.Client.ViewModel
+{
+    class WindowsPackageManagerViewModel
+    {
+        public WindowsPackageManagerViewModel()
+        {
+            LoadPackages();
+        }
+
+        public ObservableCollection<Package> Packages
+        {
+            get;
+            set;
+        }
+
+        public void LoadPackages()
+        {
+            ObservableCollection<Package> packages = new ObservableCollection<Package>();
+
+            Packages = packages;
+        }
+    }
+}
