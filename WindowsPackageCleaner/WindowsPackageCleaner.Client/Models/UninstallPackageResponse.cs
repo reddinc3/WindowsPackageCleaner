b81ada5d7ab3851f128919fae720b8e4183a0086
Kieron Drumm <klrdrumm@outlook.com>
2021-04-03 13:09:06 +0100

    Implemented package management functionality

---
+++ WindowsPackageCleaner/WindowsPackageCleaner.Client/Models/UninstallPackageResponse.cs
@@ -0,0 +1,30 @@
+﻿using Windows.ApplicationModel;
+
+namespace WindowsPackageCleaner.Client.Models
+{
+    /// <summary>
+    /// Represents a single response to a request to uninstall a Windows package.
+    /// </summary>
+    public class UninstallPackageResponse
+    {
+        /// <summary>
+        /// The error message provided, in the event that the uninstallation fails.
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// The ID of the package being uninstalled.
+        /// </summary>
+        public PackageId PackageId { get; set; }
+
+        /// <summary>
+        /// The name of the package being uninstalled.
+        /// </summary>
+        public string PackageName { get; set; }
+
+        /// <summary>
+        /// Whether or not the uninstallation succeeded.
+        /// </summary>
+        public bool Success { get; set; }
+    }
+}
